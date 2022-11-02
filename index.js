8358f257547bc71cdf8b120c07d4b90f8a0785d7
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-02 17:09:22 +0600

    credentials setting

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
+const { json } = require('express')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const port = process.env.PORT || 5000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+//!database connection details
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7incky7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Genius Car server is running!')
 })
 
 app.listen(port, () => {
